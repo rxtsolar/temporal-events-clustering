fc6bf74ef44417b43fc8779b8f31cf67c32ecbca
Guangyu Sun <guangyu.sun@oracle.com>
2015-07-28 23:39:18 -0700

    Make K adjustable

--- cooper.cpp
+++ cooper.cpp
@@ -17,72 +17,9 @@ int scale1 = 1;
 int shift1 = 0;
 int scale2 = 1;
 int shift2 = 0;
+int KK = 10000;
 double minimum = DBL_MAX;
 double maximum = 0;
-
-void plot1(int, void*)
-{
-	Mat image = Mat::zeros(768, 1024, CV_8UC3);
-	int y = image.rows / 2;
-	int x = 0;
-	int margin = image.cols / 20;
-
-	double rate = (double)(image.cols - 2 * margin) / (maximum - minimum);
-
-	for (int i = 0; i < data.size(); i++) {
-		x = margin + rate * scale1 * (data[i] - minimum);
-		x -= (double)(scale1 - 1) * (image.cols - 2 * margin) * shift1 / 100;
-		if (labels[i])
-			circle(image, Point(x, y), 2, CV_RGB(255, 0, 0));
-		else
-			circle(image, Point(x, y), 2, CV_RGB(255, 255, 0));
-	}
-	imshow("plot1", image);
-}
-
-void plot2(int, void*)
-{
-	Mat image = Mat::zeros(768, 1024, CV_8UC3);
-	int margin = image.cols / 20;
-	int x;
-	int y;
-
-	for (int i = 0; i < data.size(); i++) {
-		x = margin + i * scale2 * (image.cols - 2 * margin) / data.size();
-		x -= (double)(scale2 - 1) * (image.cols - 2 * margin) * shift2 / 100;
-		y = image.rows - margin - (double)(image.rows - 2 * margin) *
-			(data[i] - minimum) / (maximum - minimum);
-		if (labels[i])
-			line(image, Point(x, y), Point(x, image.rows - margin),
-					CV_RGB(255, 0, 0));
-		else
-			line(image, Point(x, y), Point(x, image.rows - margin),
-					CV_RGB(255, 255, 0));
-	}
-	imshow("plot2", image);
-}
-
-void draw(const vector<double>& d)
-{
-	data = d;
-
-	for (int i = 0; i < data.size(); i++) {
-		if (minimum > data[i])
-			minimum = data[i];
-		if (maximum < data[i])
-			maximum = data[i];
-	}
-
-	namedWindow("plot1");
-	namedWindow("plot2");
-	createTrackbar("scale1", "plot1", &scale1, 100, plot1);
-	createTrackbar("shift1", "plot1", &shift1, 100, plot1);
-	createTrackbar("scale2", "plot2", &scale2, 100, plot2);
-	createTrackbar("shift2", "plot2", &shift2, 100, plot2);
-	plot1(0, 0);
-	plot2(0, 0);
-	while (waitKey() != 27);
-}
 
 Mat getSimilarityMatrix(const vector<int>& times, double K)
 {
@@ -159,6 +96,69 @@ vector<double> getPeaks(const vector<double>& scores)
 	return result;
 }
 
+void plot1(int, void*)
+{
+	Mat image = Mat::zeros(768, 1024, CV_8UC3);
+	int y = image.rows / 2;
+	int x = 0;
+	int margin = image.cols / 20;
+
+	double rate = (double)(image.cols - 2 * margin) / (maximum - minimum);
+
+	for (int i = 0; i < data.size(); i++) {
+		x = margin + rate * scale1 * (data[i] - minimum);
+		x -= (double)(scale1 - 1) * (image.cols - 2 * margin) * shift1 / 100;
+		if (labels[i])
+			circle(image, Point(x, y), 2, CV_RGB(255, 0, 0));
+		else
+			circle(image, Point(x, y), 2, CV_RGB(255, 255, 0));
+	}
+	imshow("plot1", image);
+}
+
+void plot2(int, void*)
+{
+	Mat image = Mat::zeros(768, 1024, CV_8UC3);
+	int margin = image.cols / 20;
+	int x;
+	int y;
+
+	vector<double> scores = getNoveltyScores(times, KK, 4, 1);
+	data = scores;
+	//vector<double> peaks = getPeaks(scores);
+
+	for (int i = 0; i < data.size(); i++) {
+		if (minimum > data[i])
+			minimum = data[i];
+		if (maximum < data[i])
+			maximum = data[i];
+	}
+
+	for (int i = 0; i < data.size(); i++) {
+		x = margin + i * scale2 * (image.cols - 2 * margin) / data.size();
+		x -= (double)(scale2 - 1) * (image.cols - 2 * margin) * shift2 / 100;
+		y = image.rows - margin - (double)(image.rows - 2 * margin) *
+			(data[i] - minimum) / (maximum - minimum);
+		if (labels[i])
+			line(image, Point(x, y), Point(x, image.rows - margin),
+					CV_RGB(255, 0, 0));
+		else
+			line(image, Point(x, y), Point(x, image.rows - margin),
+					CV_RGB(255, 255, 0));
+	}
+	imshow("plot2", image);
+}
+
+void draw(void)
+{
+	namedWindow("plot2");
+	createTrackbar("K", "plot2", &KK, 100000, plot2);
+	createTrackbar("scale2", "plot2", &scale2, 100, plot2);
+	createTrackbar("shift2", "plot2", &shift2, 100, plot2);
+	plot2(0, 0);
+	while (waitKey() != 27);
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc < 2)
@@ -181,10 +181,7 @@ int main(int argc, char* argv[])
 		times.push_back(time);
 	}
 
-	vector<double> scores = getNoveltyScores(times, 1, 4, 1);
-	vector<double> peaks = getPeaks(scores);
-
-	draw(scores);
+	draw();
 
 	return 0;
 }
